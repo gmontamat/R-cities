aadbedf9715558e798d6b153d9818bc916d21b54
Gustavo Montamat <gmontamat@gmail.com>
2017-02-11 13:48:22 -0300

    Switched to log scale for population

--- world_cities_analysis.R
+++ world_cities_analysis.R
@@ -20,8 +20,7 @@ LoadCities <- function() {
 
 PlotCityWorldMap <- function(cities, groups = 20) {
   # Plot "world map" of cities
-  limits <- with(cities, seq(min(population), max(population)+1,
-                             length.out = groups+1))
+  limits <- quantile(cities$population, probs = log(1:groups, base = groups))
   x.range <- c(-180, 180)
   y.range <- c(-90, 90)
   plot(x = x.range, y = y.range, type = "n", xlim = x.range, ylim = y.range,
@@ -32,7 +31,7 @@ PlotCityWorldMap <- function(cities, groups = 20) {
     cities_group <- cities[cities$population>=limits[i] &
                              cities$population<limits[i+1],]
     points(cities_group$longitude, cities_group$latitude, pch = 20,
-           cex = log(i+0.1), col = colors[i])
+           cex = 0.1, col = colors[i])
   }
 }
 
@@ -47,4 +46,4 @@ SavePlot <- function(filename, plot_function, ...) {
 
 
 cities <- LoadCities()
-SavePlot("./images/world_cities.png", PlotCityWorldMap, cities)
+SavePlot("world_cities.png", PlotCityWorldMap, cities)
